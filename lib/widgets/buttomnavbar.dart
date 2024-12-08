68245a98492898ffa8af05830492cba8bf0a4043
Deuterrr <bagaspri4111@gmail.com>
2024-12-08 09:11:42 +0700

    Buttom Navbar
    Change Ui in buttomnavbar.dart from using ButtomNavigationBarItem() to just children of Row().

--- lib/widgets/buttomnavbar.dart
+++ lib/widgets/buttomnavbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Buttomnavbar extends StatefulWidget {
   final Function(int) onItemTap;
@@ -15,66 +15,55 @@ class _ButtomnavbarState extends State<Buttomnavbar> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: Container(
-        color: Color(0xffF5F0E0), // Background color of the container
-        padding: EdgeInsets.symmetric(horizontal: 25), // Add padding
-
-        child: Theme(
-          data: Theme.of(context).copyWith(
-            splashFactory: NoSplash.splashFactory, // Remove ripple effect
-            highlightColor: const Color.fromARGB(
-                0, 194, 113, 113), // Disable highlight on hover
-          ),
-          child: BottomNavigationBar(
-            elevation: 0, // Remove the shadow
-            backgroundColor: Colors
-                .transparent, // Keep it transparent to use container color
-            currentIndex: _selectedIndex,
-            onTap: (index) {
-              widget.onItemTap(index);
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            type: BottomNavigationBarType.fixed,
-            items: [
-              BottomNavigationBarItem(
-                  icon: buildNavIcon('assets/icon/home.svg', 0), label: ''),
-              BottomNavigationBarItem(
-                  icon: buildNavIcon('assets/icon/ticket.svg', 1), label: ''),
-              BottomNavigationBarItem(
-                  icon: buildNavIcon('assets/icon/favorite.svg', 2), label: ''),
-              BottomNavigationBarItem(
-                  icon: buildNavIcon('assets/icon/transaction.svg', 3),
-                  label: ''),
-              BottomNavigationBarItem(
-                  icon: buildNavIcon('assets/icon/profile.svg', 4), label: '')
-            ],
+    return Container(
+      height: 62,
+      padding: const EdgeInsets.fromLTRB(36, 5, 36, 10),
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 245, 240, 224), //cream flat
+          border: const Border(
+            top: BorderSide(
+              color: Color.fromARGB(255, 14, 37, 34),
+              width: 1.2,
+            ),
           ),
         ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          buildNavIcon('assets/icon/home.svg', 0),
+          buildNavIcon('assets/icon/ticket.svg', 1),
+          buildNavIcon('assets/icon/favorite.svg', 2),
+          buildNavIcon('assets/icon/transaction.svg', 3),
+          buildNavIcon('assets/icon/profile.svg', 4),
+        ],
       ),
     );
   }
 
   Widget buildNavIcon(String path, int index) {
-    return Container(
-      width: 42,
-      height: 42,
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      decoration: BoxDecoration(
-          color:
-              _selectedIndex == index ? Color(0xffFFFDF7) : Colors.transparent,
-          borderRadius: BorderRadius.circular(8)),
-      child: Center(
-        child: SvgPicture.asset(
-          path,
-          // width: 19,
-          // height: 19,
-          colorFilter: _selectedIndex == index
-              ? ColorFilter.mode(Color(0xffDC555E), BlendMode.srcIn)
-              : null,
+    return GestureDetector(
+      onTap: () {
+        widget.onItemTap(index);
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      child: Container(
+        width: 42,
+        height: 42,
+        decoration: BoxDecoration(
+          color: _selectedIndex == index ? const Color(0xffFFFDF7) : Colors.transparent,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: SvgPicture.asset(
+            path,
+            width: 22,
+            height: 22,
+            colorFilter: _selectedIndex == index
+                ? const ColorFilter.mode(Color(0xffDC555E), BlendMode.srcIn)
+                : null,
+          ),
         ),
       ),
     );
